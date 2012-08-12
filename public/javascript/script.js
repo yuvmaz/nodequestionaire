871cde7ae6715621be551b5dd03fc878bd0f1553
Yuval Mazor <yuvmaz@gmail.com>
2012-08-12 13:46:35 +0300

    Show a form, using JQueryUI

---
+++ public/javascript/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#settingsLink').click(function() {
+        var myDialog = $('#settingsDialog')
+            .dialog({ autoOpen: false,  buttons: { "OK":  function() {}, "Cancel": function() {} }});
+        myDialog.dialog('open');
+        return false;
+    });
+});
