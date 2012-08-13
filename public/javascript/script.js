24000822407a3d6063bf6d86d336755faf9c35d2
Yuval Mazor <yuvmaz@gmail.com>
2012-08-13 22:02:07 +0300

    Show language buttons in Select Languages dialog

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,10 +1,21 @@
+var selectedLanguage;
+
+
+
 $(document).ready(function() {
     $('#settingsLink').click(function() {
         var myDialog = $('#settingsDialog')
-            .dialog({ autoOpen: false,  buttons: { "OK":  function() {}, "Cancel": function() {} }});
+            .dialog({ autoOpen: false, title: 'Select Languages', width:400,  buttons: { "OK":  function() {}, "Cancel": function() {} }});
         myDialog.dialog('open');
         return false;
     });
 
-    $('#languageBox').autocomplete({source:  "/getLanguages", minLength: 3});
+    $('#languageBox').autocomplete({
+        source:  "/getLanguages", minLength: 3,
+        select: function(ev, ui) {
+            var language = ui.item.value,
+            span = $('<span>').text(language).append('<a href="#">Close</a>');
+            $('#languageList').append(span);
+        }
+    });
 });
