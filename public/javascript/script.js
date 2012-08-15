9541c5d4c90a8cef83023cf621d65679c764716d
Yuval Mazor <yuvmaz@gmail.com>
2012-08-15 16:51:49 +0300

    Implement JQuery code to remove existing languages in settings dialog

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -14,8 +14,18 @@ $(document).ready(function() {
         source:  "/getLanguages", minLength: 3,
         select: function(ev, ui) {
             var language = ui.item.value,
-            span = $('<span>').text(language).append('<a href="#">Close</a>');
+            span = $('<span>').text(language),
+            a = $("<a>").addClass("remove").attr({
+                href: "javascript:",
+                title: "Remove " + language
+            }).text("x").appendTo(span); 
             $('#languageList').append(span);
         }
     });
+
+    $(".remove", document.getElementById("settingsDialog")).live("click", function(){
+        $(this).parent().remove();
+    });
+
+    
 });
